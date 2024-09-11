845e0fa5f1995a3db499367c5d8867d8dbd84087
JoaoTorpe <victotorpe10@gmail.com>
2024-09-11 14:26:42 -0300

    Endpoints iniciais

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['POST'])
+def login(request):
+    return Response({"login"})
+
+@api_view(['POST'])
+def register(request):
+    return Response({"register"})
+
+
+@api_view(['POST'])
+def validate_token(request):
+    return Response({"validate"})
