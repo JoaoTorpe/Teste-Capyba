554ec6196602bdf88aab7493d0588f8fa19c6ac8
JoaoTorpe <victotorpe10@gmail.com>
2024-09-12 14:44:07 -0300

    Endpoint para recuperar filmes

--- movieapp/views.py
+++ movieapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import FilmeSerializer
+from . models import Filme
+@api_view(['GET'])
 
-# Create your views here.
+def filmes(request):
+    filmes = Filme.objects.all()
+    ser = FilmeSerializer(filmes,many=True)
+    return Response(ser.data)
