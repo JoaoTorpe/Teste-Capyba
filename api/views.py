5cb5443a414179bbeaf245ae58e3e010e9105ded
JoaoTorpe <victotorpe10@gmail.com>
2024-09-11 22:23:03 -0300

    Endpoint de logout

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import  TokenAuthentication
 from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -11,10 +12,14 @@ from django.shortcuts import get_object_or_404
 @api_view(['POST'])
 def login(request):
     user = get_object_or_404(User,username=request.data['username'])
+
     if user.check_password(request.data['password']):
         return Response(status=status.HTTP_404_NOT_FOUND)
-    token = Token.objects.create(user=user)
+    
+    token, created = Token.objects.get_or_create(user=user)
     return Response({"token":token.key})
+
+
 @api_view(['POST'])
 def register(request):
     ser = UserSerializer(data = request.data)
@@ -25,8 +30,17 @@ def register(request):
         user.set_password(request.data['password'])
         user.save()
         return Response(status=status.HTTP_201_CREATED)
+    
     return Response(ser.errors,status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-def validate_token(request):
-    return Response({"validate"})
+@permission_classes([IsAuthenticated])
+def logout(request):
+   try:
+       token = Token.objects.get(user=request.user)
+       
+       token.delete()
+       return Response({"detail": "Logout realizado com sucesso."}, status=status.HTTP_200_OK)
+   except Token.DoesNotExist:
+       return Response({"detail": "Token não encontrado."}, status=status.HTTP_400_BAD_REQUEST)
+   
