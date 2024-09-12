f4142143b83775878650e8d0b08baa0f8fee36f7
JoaoTorpe <victotorpe10@gmail.com>
2024-09-12 14:23:48 -0300

    Model Filme

--- movieapp/models.py
+++ movieapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField(max_length=255)  
+    genero = models.CharField(max_length=100)  
+    premiado = models.BooleanField(default=False)  
+    ano_de_lancamento = models.PositiveIntegerField()  
+
+    def __str__(self):
+        return self.titulo
