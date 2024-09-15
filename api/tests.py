3f337417667096248f60ce46de1749c4f30a91f3
JoaoTorpe <victotorpe10@gmail.com>
2024-09-15 20:46:37 -0300

    Test: teste endpoint register, falha por falta de atributos

--- api/tests.py
+++ api/tests.py
@@ -39,3 +39,7 @@ class TestesAutenticacao(APITestCase):
     def test_register_sucesso(self):
        response = self.client.post(reverse('register'),{'username':'novo_user','email':'user@hotmail.com','password':'senha'})
        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+
+    def test_register_falha_falta_atributos(self):
+       response =  self.client.post(reverse('register'),{'password':'senha'})  
+       self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
