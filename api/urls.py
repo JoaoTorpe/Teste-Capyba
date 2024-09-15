a4e300fd48df37d79701955f83a92022fdf28438
JoaoTorpe <victotorpe10@gmail.com>
2024-09-15 20:28:22 -0300

    Adicionando 'name' nas urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login', views.login ),
-    path('register',views.register),
-    path('logout',views.logout),
-    path('changepass',views.change_password),
-    path('updatedata',views.update_user_data)
+    path('login', views.login,name='login' ),
+    path('register',views.register,name='register'),
+    path('logout',views.logout,name='logout'),
+    path('changepass',views.change_password,name='changepass'),
+    path('updatedata',views.update_user_data,name='updatedata')
 ]
