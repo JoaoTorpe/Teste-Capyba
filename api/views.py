062c11caa7ab9b7f5f3364325d3ae23ec8c0a7c1
JoaoTorpe <victotorpe10@gmail.com>
2024-09-16 16:46:16 -0300

    Docs: Adicionando exemplos aos endpoints do Swagger

--- api/views.py
+++ api/views.py
@@ -2,12 +2,31 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer ,UserUpdateSerializer
+from .serializers import UserSerializer ,UserUpdateSerializer,LoginSerializer,RegisterSerializer,ChangepassSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from drf_spectacular.utils import extend_schema,OpenApiParameter, OpenApiExample, inline_serializer, OpenApiRequest,OpenApiResponse
 from django.shortcuts import get_object_or_404
 
+@extend_schema(
+    summary="Login do usuário (Lembrar do prefixo 'Token')",
+    description="Sempre que for colocar o token no value do Authrize tem que por o prefixo 'Token' Ex: Token 376a5dbefd1c7f5eaca4554df4ac0cbff47",
+    request=OpenApiRequest(
+        request=LoginSerializer,  
+        examples=[
+            OpenApiExample(
+                name='Exemplo de login',
+                value={
+                    'email': 'email@email.com',
+                    'password': 'senha123'
+                },
+                description='Exemplo de login com email e senha'
+            )
+        ]
+    )
+)
+ 
 @api_view(['POST'])
 def login(request):
     user = get_object_or_404(User,email=request.data['email'])
@@ -18,7 +37,24 @@ def login(request):
     token, created = Token.objects.get_or_create(user=user)
     return Response({"token":token.key})
 
-
+@extend_schema(
+    summary="Registro do usuário",
+    description="Endpoint para registro do usuário",
+    request=OpenApiRequest(
+        request=RegisterSerializer,  
+        examples=[
+            OpenApiExample(
+                name='Exemplo de registro',
+                value={
+                    'username':'joao',
+                    'email': 'email@email.com',
+                    'password': 'senha123'
+                },
+                description='Exemplo de registro'
+            )
+        ]
+    )
+)
 @api_view(['POST'])
 def register(request):
     ser = UserSerializer(data = request.data)
@@ -32,6 +68,11 @@ def register(request):
     
     return Response(ser.errors,status=status.HTTP_400_BAD_REQUEST)
 
+@extend_schema(
+    summary="Logout",
+    description="Esse endpoint não precisa de parametros é só executar",
+    
+)
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def logout(request):
@@ -43,7 +84,23 @@ def logout(request):
    except Token.DoesNotExist:
        return Response({"detail": "Token não encontrado."}, status=status.HTTP_400_BAD_REQUEST)
    
-
+@extend_schema(
+    summary="Mudar senha do usuario atual",
+    description="Endpoint para alterar a senha do usuario com a confirmacao da senha atual",
+    request=OpenApiRequest(
+        request=ChangepassSerializer,  
+        examples=[
+            OpenApiExample(
+                name='Exemplo mudar senha',
+                value={
+                    'current_password': 'senha123',
+                    'new_password': 'pass123'
+                },
+                description='Exemplo de troca de senha'
+            )
+        ]
+    )
+)
  #Muda a senha do usuario logado, com a confirmacao da senha atual  
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])  
@@ -64,6 +121,23 @@ def change_password(request):
 
     return Response({"success": "A senha foi alterada com sucesso!"}, status=status.HTTP_200_OK)
 
+@extend_schema(
+    summary="Alterar dados ",
+    description="Endpoint para alterar dados do usuario (Username e Email)",
+    request=OpenApiRequest(
+        request=ChangepassSerializer,  
+        examples=[
+            OpenApiExample(
+                name='Exemplo de alteracao de dados',
+                value={
+                    'username': 'novo_username',
+                    'email': 'novo@email.com'
+                },
+                description='Exemplo de alteracao de dados'
+            )
+        ]
+    )
+)
 #Endpoint para alterar cadastro de usuario, muda o nome e o email do usuario atual
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
