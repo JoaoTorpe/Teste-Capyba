aa032bc43cc5a00c9c77a24a4883c55434ee57fc
JoaoTorpe <victotorpe10@gmail.com>
2024-09-16 16:45:06 -0300

    Docs: Serializers para documentacao

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,18 @@ class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ['username','email']
+
+#Serializers para configurar o OpenApi
+class LoginSerializer(serializers.Serializer):
+        email = serializers.EmailField()
+        password = serializers.CharField()
+
+class RegisterSerializer(serializers.Serializer):
+        unsername = serializers.CharField() 
+        email = serializers.EmailField()
+        password = serializers.CharField()
+
+class ChangepassSerializer(serializers.Serializer):
+        current_password = serializers.CharField() 
+        new_password = serializers.CharField()
+        
