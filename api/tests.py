03d05a9dd33187f5b422055b6d45df59def91479
JoaoTorpe <victotorpe10@gmail.com>
2024-09-15 21:00:22 -0300

    Test: teste endpoint change_password, falha pois a senha atual esta errada

--- api/tests.py
+++ api/tests.py
@@ -43,3 +43,9 @@ class TestesAutenticacao(APITestCase):
     def test_register_falha_falta_atributos(self):
        response =  self.client.post(reverse('register'),{'password':'senha'})  
        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_change_password_falha_senha_atual_incorreta(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('changepass'),{"current_password":"senhaerrada","new_password":"1234"})
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+
